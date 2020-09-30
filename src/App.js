3e0f1a17b7fdde668977320a25927e0bbff92488
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 12:11:06 +0530

    Add router in app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Manager from './components/Manager';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Manager />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Manager />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
