0845821992e3d083a7f70f686fdc528028197e75
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 12:19:29 +0530

    Add on click handler for button

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Manager() {
+    const history = useHistory();
+
     return (
         <div className="manager">
             <h5>Your Bookmarks</h5>
 
-            <button className="manager__addButton">
+            <button className="manager__addButton" onClick={() => history.push('/search')}>
                 Add new bookmark
             </button>
         </div>
