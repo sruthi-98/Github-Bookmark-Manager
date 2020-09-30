1b7c245ec8439f2bc4f2897a89fbc962c43cb6fb
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 11:55:20 +0530

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <h3>Github Bookmark Manager</h3>
+        </div>
+    )
+}
+
+export default Header;
