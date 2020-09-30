28f19be85336f38474e3f16455b9003de862add3
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 18:27:54 +0530

    Add function for displaying repo result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function Result() {
+function Result({result, searchType}) {
+    console.log(result);
+
+    const repoResults = () => {
+        return  (result?.data?.items.map((item) => {
+            return <p>{item.name}</p>
+        }));
+    }
+
+    const repo = repoResults();
+
     return (
         <div className="result">
-            
+            {searchType === 'repo' && repo}
         </div>
     )
 }
