9e5893fa3b0fa8343c38de6b14bc8b9e8c650293
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 11:59:35 +0530

    Create manager component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Manager from './components/Manager';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Manager />
     </div>
   );
 }
---
+++ src/components/Manager.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Manager() {
+    return (
+        <div className="manager">
+            <h5>Your Bookmarks</h5>
+
+            <button className="manager__addButton">
+                Add new bookmark
+            </button>
+        </div>
+    )
+}
+
+export default Manager;
