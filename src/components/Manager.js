8c395e4882646bdcd88f56ba00a215809d117ad6
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 22:25:23 +0530

    Display bookmarked repos

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useBookmarkValue } from '../BookmarkContext';
 
 function Manager() {
     const history = useHistory();
+    const [{ bookmark }, dispatch] = useBookmarkValue();
 
     return (
         <div className="manager">
             <h5>Your Bookmarks</h5>
-
+            <div className="manager__list">
+                {
+                    bookmark.length === 0 ?
+                    <p>No bookmarks added</p> :
+                    bookmark.map(item => <p>{item.repo_name}</p>)
+                }
+            </div>
             <button className="manager__addButton" onClick={() => history.push('/search')}>
                 Add new bookmark
             </button>
