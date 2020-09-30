6501b006d48af6bba4fe01cec73d1d5403f55059
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 12:12:12 +0530

    Create search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div className="search">
+            <h3>Search component</h3>
+        </div>
+    )
+}
+
+export default Search;
