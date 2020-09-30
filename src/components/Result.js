ecec88447ad71e8c39d5b057a05a40dcd5ac3542
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-09-30 20:38:24 +0530

    Create function for adding repo

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useBookmarkValue } from '../BookmarkContext';
 
 function Result({result, searchType}) {
+    const [{ bookmark }, dispatch] = useBookmarkValue();
+
+    const addRepo = (name) => {
+        dispatch({
+            type: 'ADD_BOOKMARK',
+            item: {
+                repo_name: name
+            }
+        });
+
+        console.log(bookmark);
+    }
 
     const repoResults = () => {
         return  (result?.data?.items.map((item) => {
-            return <p>{item.name}</p>
-        }));
+            return (
+                <div className="result__repo">
+                    <p>{item.name}</p>
+                    <button onClick={e => addRepo(item.name, e)}>Add</button>
+                </div>
+        )}));
     }
 
     const userResults = () => {
