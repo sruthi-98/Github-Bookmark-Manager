44a63b0e656c19fc02a3ad3cf5a587aad02aa818
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-01 12:53:16 +0530

    Include repo dependent code

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useBookmarkValue } from '../BookmarkContext';
 
-function Repo() {
+function Repo({ repo }) {
+    const [{ bookmark }, dispatch] = useBookmarkValue();
+
+    const addRepo = (repo) => {
+        dispatch({
+            type: 'ADD_BOOKMARK',
+            item: {
+                repo_name: repo.name,
+                url: repo.html_url
+            }
+        });
+    }
+
     return (
         <div className="repo">
-            
+            <p>{repo.name}</p>
+            <button onClick={e => addRepo(repo, e)}>Add</button>
         </div>
     )
 }
