1d71f0970c7e12d5982ec575f41845b9adc74a96
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-01 12:33:14 +0530

    Add click handler for view repository button

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from '../axios';
 
 function User({ user }) {
+
+    const listRepo = () => {
+        axios({
+            url: '/users/' + user.login + '/repos',
+        }).then((result) => console.log(result))
+          .catch(error => console.log(error));
+    }
+
     return (
         <div className="user">
-            
+            <p>{user.login}</p>
+            <button onClick={listRepo}>View {user.login}'s repository list</button>
         </div>
     )
 }
