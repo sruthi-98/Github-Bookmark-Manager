10e4c95326c775df013861de28519763ceb8cbd4
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-01 12:52:01 +0530

    Include repo component and remove repo dependent code

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { useBookmarkValue } from '../BookmarkContext';
+import Repo from './Repo';
 import User from './User';
 
 function Result({result, searchType}) {
-    const [{ bookmark }, dispatch] = useBookmarkValue();
-
-    const addRepo = (repo) => {
-        dispatch({
-            type: 'ADD_BOOKMARK',
-            item: {
-                repo_name: repo.name,
-                url: repo.html_url
-            }
-        });
-    }
 
     const repoResults = () => {
         return  (result?.data?.items.map((item) => {
             return (
-                <div className="result__repo">
-                    <p>{item.name}</p>
-                    <button onClick={e => addRepo(item, e)}>Add</button>
-                </div>
+               <Repo repo={item} />
         )}));
     }
 
