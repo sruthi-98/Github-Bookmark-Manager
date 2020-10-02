116f8c910373f9b32187d740d4ea185c68ff5bfc
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-02 20:33:46 +0530

    Add function for saving repo

--- src/components/AddTitle.js
+++ src/components/AddTitle.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import { useBookmarkValue } from '../BookmarkContext';
 
-function AddTitle() {
+function AddTitle({ repo }) {
     const [title, setTitle] = useState('');
+
+    const [{ bookmark }, dispatch] = useBookmarkValue();
+    
+    const addRepo = (repo) => {
+        dispatch({
+            type: 'ADD_BOOKMARK',
+            item: {
+                repo_name: repo.name,
+                title: title,
+                url: repo.html_url
+            }
+        });
+    }
 
     return (
         <div>
@@ -11,7 +25,7 @@ function AddTitle() {
                     type="text"
                     value={title}
                     onChange={e => setTitle(e.target.value)} />
-                <button>Save</button>
+                <button  onClick={e => addRepo(repo, e)}>Save</button>
             </div>
         </div>
     )
