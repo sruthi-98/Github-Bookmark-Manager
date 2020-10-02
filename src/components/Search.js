a61e3cc4b5240076e4c2c00746b54df41964b47d
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-02 16:14:37 +0530

    Add automatic scroll and display current page number

--- src/components/Search.js
+++ src/components/Search.js
@@ -34,7 +34,10 @@ function Search() {
                         page: pageNumber,
                         per_page: 20
                     }
-                }).then((result) => setResult(result))
+                }).then((result) => {
+                        setResult(result);
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                    })
                   .catch(error => console.log(error));
                 break;
 
@@ -46,48 +49,61 @@ function Search() {
                         page: pageNumber,
                         per_page: 20
                     }
-                }).then((result) => setResult(result))
+                }).then((result) => {
+                        setResult(result);
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                    })
                   .catch(error => console.log(error));
                 break;
         
             default:
                 break;
         }
+        
     }
 
+    console.log(result);
+    console.log(Object.keys(result).length);
 
     return (
-        <div className="h-screen p-5">
-            <div className="flex bg-white h-auto w-full px-4 py-2 rounded">
+        <div className="h-auto p-5">
+            <div className="block md:flex bg-white h-auto w-full px-4 py-2 rounded">
                 <input
                     type="text"
                     value={searchValue}
                     placeholder="Search"
                     onChange={e => setSearchValue(e.target.value)}
-                    className="flex-auto border border-solid border-gray-500 placeholder-black m-2 p-2 rounded"
+                    className="block w-full md:flex-auto border border-solid border-gray-500 placeholder-black m-2 p-2 rounded"
                 />
 
                 <select 
                     onChange={findSearchType}
-                    className="search__dropdown flex-auto bg-gray-200 border border-gray-200 text-gray-700 m-2 p-2" 
-                >
+                    className="search__dropdown block w-full md:flex-auto bg-gray-200 border border-gray-200 text-gray-700 m-2 p-2" 
+                > 
                     <option value="users">{searchValue} in Users</option> 
                     <option value="repo">{searchValue} in Repositories</option>
                 </select>
 
                 <button 
-                    className="bg-indigo-700 hover:bg-indigo-800 font-semibold text-white w-1/12 m-2 p-2 rounded"
+                    className="bg-orange-600 hover:bg-orange-700 font-semibold text-white md:w-2/12 m-2 p-2 rounded"
                     onClick={fetchResult}
                 >
                     Search
                 </button>
             </div>
 
+            {
+                Object.keys(result).length !== 0 && 
+                <p className="m-2 px-4 py-2">
+                    Showing page <strong>{pageNumber}</strong> of results.
+                </p>
+            }
+
             <Result result={result} searchType={searchType}/>
 
             {
                 Object.keys(result).length !== 0 && 
-                <div className="flex justify-end p-3">
+                <div className="flex justify-end p-3 mb-5">
                     <button 
                         onClick={() => setPageNumber(pageNumber > 1 ? pageNumber - 1 : pageNumber)}
                         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
