38e5481d025ab8ed15b809111db4ad642a4d1022
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-02 16:29:52 +0530

    Toggle state of button after result fetch

--- src/components/User.js
+++ src/components/User.js
@@ -7,21 +7,23 @@ function User({ user }) {
     const [clicked, setClicked] = useState(false);
 
     const listRepos = () => {
-        setClicked(!clicked);
         axios({
             url: '/users/' + user.login + '/repos',
-        }).then((result) => setRepos(result))
+        }).then((result) => {
+            setRepos(result);
+            setClicked(!clicked);
+        })
           .catch(error => console.log(error));
     }
 
     return (
-        <div className="bg-white border border-solid border-gray-400 shadow-md p-5 m-5 rounded">
+        <div className="bg-white border border-solid border-gray-400 shadow-lg p-5 m-5 rounded">
             <p className="mb-4">
                 <strong>User:</strong> {user.login}
             </p>
             <button 
                 onClick={listRepos}
-                className="bg-transparent border border-solid border-indigo-800 font-semibold text-indigo-800 p-2 rounded hover:shadow-lg"
+                className="bg-transparent border border-solid border-purple-800 font-semibold text-indigo-800 p-2 rounded hover:shadow-lg"
             >
                 {!clicked ? 'View' : 'Hide'} <strong>{user.login}</strong>'s repository list
             </button>
