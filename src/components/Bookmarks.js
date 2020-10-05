0bbccc6fde8a40b73af06238e5d55d8cd3862d44
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-05 18:55:03 +0530

    Update styling

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -16,10 +16,12 @@ function Bookmarks() {
                 <p className="text-xl p-2 mt-4">No bookmarks added !!!!</p> :
                 bookmark.map((item, i) => (
                     <div  key={i} className="flex justify-between items-center bg-white border border-solid border-gray-400 shadow-md p-5 m-5 rounded">
-                        {item.title !== '' && <p><strong>Title: </strong>{item.title}</p>}
-                        <a href={item.url} target="_blank" rel="noopener noreferrer">
-                            <strong>Repository: </strong>{item.repo_name}
-                        </a>
+                        <div className="flex flex-col">
+                            {item.title !== '' && <p><strong>Title: </strong>{item.title}</p>}
+                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                <strong>Repository: </strong>{item.repo_name}
+                            </a>
+                        </div>
                         <button
                             onClick={e => deleteRepo(item.id, e)}
                             className="bg-purple-800 hover:bg-purple-900 font-semibold text-white h-10 py-2 px-4 rounded"
