95a124d11d23fdfb15c92ac5f26a1aeff446ca8d
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-05 18:51:55 +0530

    Add reference for title input and Include popup component

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useBookmarkValue } from '../BookmarkContext';
+import Popup from './Popup';
 
 function Repo({ repo }) {
     const [showPopup, setShowPopup] = useState(false);
     const [title, setTitle] = useState('');
+    const titleRef = useRef('')
 
-    const [{ bookmark }, dispatch] = useBookmarkValue();
+    const [, dispatch] = useBookmarkValue();
     
     const addRepo = (repo) => {
         setShowPopup(false);
@@ -18,6 +20,10 @@ function Repo({ repo }) {
                 url: repo.html_url
             }
         });
+    }
+
+    const updateTitle = () => {
+        setTitle(titleRef.current.value);
     }
 
     return (
@@ -35,24 +41,7 @@ function Repo({ repo }) {
             </div>
 
             {/* Shows a popup to enter a title for the selected bookmark */}
-            {showPopup && 
-                <div className="fixed z-10 h-screen w-screen bg-black bg-opacity-75 top-0 left-0">
-                    <div className="popup absolute flex flex-col bg-orange-100 h-auto w-3/5 lg:w-1/4 border border-solid border-black p-12">
-                        <p className="flex text-lg">Enter a title (optional):</p>
-                        <input 
-                            type="text"
-                            value={title}
-                            onChange={e => setTitle(e.target.value)} 
-                            className="block w-full md:flex-auto border border-solid border-gray-500 placeholder-black p-2 mb-4 rounded"
-                        />
-                        <button 
-                            onClick={e => addRepo(repo, e)}
-                            className="bg-orange-600 hover:bg-orange-700 font-semibold text-white py-2 px-5 rounded" 
-                        >
-                            Save
-                        </button>
-                    </div>
-                </div>}
+            {showPopup && <Popup repo={repo} titleRef={titleRef} changeHandler={updateTitle} addRepo={addRepo} />}
         </div>
     )
 }
