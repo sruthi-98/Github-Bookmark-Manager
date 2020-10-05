142ab260cb3a02f43a31f1a7df51def3229ebecb
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-05 16:59:43 +0530

    Add dropdown element

--- src/components/SearchDropdown.js
+++ src/components/SearchDropdown.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function SearchDropdown() {
     return (
-        <div>
-            
-        </div>
+        <select className="search__dropdown block w-full md:flex-auto bg-gray-200 border border-gray-200 text-gray-700 m-2 p-2"> 
+            <option value="users">in Users</option> 
+            <option value="repo">in Repositories</option>
+        </select>
     )
 }
 
