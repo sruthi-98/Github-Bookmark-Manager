736560878bb6437173491254343430c310a06515
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-05 16:47:33 +0530

    Refactor getResult function

--- src/components/Result.js
+++ src/components/Result.js
@@ -8,22 +8,10 @@ function Result({result, searchType}) {
             return <p className="text-xl m-2 px-4 py-2">No results !!!</p>
         }
         else {
-            switch (searchType) {
-                case 'users':
-                    return  (result?.data?.items.map((item) => {
-                        return ( 
-                            <User key={item.id} user={item} />
-                    )}));
-    
-                case 'repo':
-                    return  (result?.data?.items.map((item) => {
-                        return (
-                           <Repo key={item.id} repo={item} />
-                    )}));
-    
-                default:
-                    break;
-            }
+            return  (result?.data?.items.map((item) => {
+                return ( 
+                    searchType === 'repo' ? <Repo key={item.id} repo={item} /> : <User key={item.id} user={item} />
+            )}));
         }
     }
 
