89d01cce25b5d0e1526f4014886d05668b0096e3
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-08 15:04:36 +0530

    Add unique ID and required attribute

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,8 +5,10 @@ function SearchBar({ searchRef }) {
         <input
             type="text"
             ref={searchRef}
+            id="searchBar"
             placeholder="Search"
-            className="block w-full md:flex-auto border border-solid border-gray-500 placeholder-black m-2 p-2 rounded"
+            className={`block w-full md:flex-auto border border-solid border-gray-500 placeholder-black m-2 p-2 rounded`}
+            required={true}
         />
     )
 }
