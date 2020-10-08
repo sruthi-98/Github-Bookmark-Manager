fb3ce762d693241b84c683bab74b6d3652e19350
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-08 15:03:58 +0530

    Change button element to input element of type submit

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function SearchButton({ clickHandler }) {
+function SearchButton() {
     return (
-        <button 
+        <input
+            type="submit"
+            value="Search"
             className="bg-orange-600 hover:bg-orange-700 font-semibold text-white md:w-2/12 m-2 p-2 rounded"
-            onClick={clickHandler}
-        >
-            Search
-        </button>
+        />
     )
 }
 
