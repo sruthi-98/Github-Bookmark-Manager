ef6ffc37934174514526bddad5ef3b531be779b9
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-08 17:19:39 +0530

    Add onChange handler for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function SearchBar({ searchRef }) {
+function SearchBar({ onChange }) {
     return (
         <input
             type="text"
-            ref={searchRef}
             id="searchBar"
             placeholder="Search"
+            onChange={onChange}
             className={`block w-full md:flex-auto border border-solid border-gray-500 placeholder-black m-2 p-2 rounded`}
             required={true}
         />
