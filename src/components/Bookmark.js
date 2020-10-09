fbf5f6d58e109bf38ad04a571d0ec0db0d97cc76
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-09 17:28:23 +0530

    Perform component decomposition

---
+++ src/components/Bookmark.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useBookmarkValue } from '../BookmarkContext';
+
+function Bookmark({ bookmark, id}) {
+    const [, dispatch] = useBookmarkValue();
+
+    const deleteRepo = (id) => {
+        dispatch({
+            type: 'DELETE_BOOKMARK',
+            id: id
+        });
+    }
+
+    return (
+        <div key={id} className="flex justify-between bookmarks-center bg-white border border-solid border-gray-400 shadow-md p-5 m-5 rounded">
+            <div className="flex flex-col">
+                {bookmark.title !== '' && <p><strong>Title: </strong>{bookmark.title}</p>}
+                <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
+                    <strong>Repository: </strong>{bookmark.repo_name}
+                </a>
+            </div>
+            <button
+                onClick={e => deleteRepo(bookmark.id, e)}
+                className="bg-purple-800 hover:bg-purple-900 font-semibold text-white h-10 py-2 px-4 rounded"
+            >
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default Bookmark;
--- src/components/BookmarkList.js
+++ src/components/BookmarkList.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import { useBookmarkValue } from '../BookmarkContext';
+import Bookmark from './Bookmark';
 
 function BookmarkList() {
-    const [{ bookmark }, dispatch] = useBookmarkValue();
+    const [{ bookmark }, ] = useBookmarkValue();
 
-    const deleteRepo = (id) => {
-        dispatch({
-            type: 'DELETE_BOOKMARK',
-            id: id
-        });
-    }
     return (
         <div>
             {bookmark.length === 0 ?
                 <p className="text-xl p-2 mt-4">No bookmarks added !!!!</p> :
-                bookmark.map((item, i) => (
-                    <div key={i} className="flex justify-between items-center bg-white border border-solid border-gray-400 shadow-md p-5 m-5 rounded">
-                        <div className="flex flex-col">
-                            {item.title !== '' && <p><strong>Title: </strong>{item.title}</p>}
-                            <a href={item.url} target="_blank" rel="noopener noreferrer">
-                                <strong>Repository: </strong>{item.repo_name}
-                            </a>
-                        </div>
-                        <button
-                            onClick={e => deleteRepo(item.id, e)}
-                            className="bg-purple-800 hover:bg-purple-900 font-semibold text-white h-10 py-2 px-4 rounded"
-                        >
-                            Delete
-                        </button>
-                    </div>
-            ))}
+                bookmark.map((item, i) => <Bookmark bookmark={item} id={i} />)}
         </div>
     )
 }
