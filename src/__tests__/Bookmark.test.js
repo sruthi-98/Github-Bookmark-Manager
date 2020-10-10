ae0479b97bf11d0ba56dcf9ee0b3aab05956c341
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 20:01:16 +0530

    Include Context Provider

--- src/__tests__/Bookmark.test.js
+++ src/__tests__/Bookmark.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { BookmarkProvider } from '../BookmarkContext';
+import reducer, { initialState } from '../Reducer';
 import Bookmark from '../components/Bookmark';
 
 describe('Bookmark Component', () => {
     test('Check delete button click calls the onClick handler', () => {
         const onClickHandler = jest.fn();
-        const wrapper = mount(<Bookmark />);
+        const wrapper = mount(
+            <BookmarkProvider reducer={reducer} initialState={initialState}>
+                <Bookmark />
+            </BookmarkProvider>
+        );
         const button = wrapper.find('button');
         button.setProps({
             onClick: onClickHandler
