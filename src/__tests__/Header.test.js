b0ee3639ebe4054db006adb5fe7c7cb2cc427638
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 17:56:02 +0530

    Ensure header links to home page

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,11 +4,13 @@ import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header';
 
 describe('Header', () => {
-    test('Check if header component is rendered correctly', () => {
+    test('Check if header component is rendered correctly and links to the Home page', () => {
         const wrapper = mount(
             <MemoryRouter>
                 <Header />
             </MemoryRouter>
         );
+        const link = wrapper.find('Link');
+        expect(link.props()).toHaveProperty('to', '/');
     })
 })
