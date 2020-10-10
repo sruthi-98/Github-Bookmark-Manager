d459be61950b78cc1e5dcf035dd17ba06cc5f593
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 17:52:54 +0530

    Include MemoryRouter to render correctly

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header';
 
 describe('Header', () => {
     test('Check if header component is rendered correctly', () => {
-        const wrapper = mount(<Header />);
+        const wrapper = mount(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        );
     })
 })
