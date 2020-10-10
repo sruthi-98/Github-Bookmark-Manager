dab23cfa1563c622116f34547736b053b1520a35
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 22:25:53 +0530

    Include useHistory mock

--- src/__tests__/AddBookmark.test.js
+++ src/__tests__/AddBookmark.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { mount } from 'enzyme';
 import AddBookmark from '../components/AddBookmark';
 
+// Mock for useHistory
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
 describe('Add Bookmark', () => {
-    test('Check if AddBookmark component is rendered correctly', () => {
+    test('Check if AddBookmark component is rendered correctly and redirects to /search when clicked', () => {
         const wrapper = mount(<AddBookmark />);
+        wrapper.find('button').simulate('click');
+        expect(mockHistoryPush).toHaveBeenCalledWith('/search');
     })
 })
