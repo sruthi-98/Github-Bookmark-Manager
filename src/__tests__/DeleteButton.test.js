326912ea32ca5c323fd63813e7044be7854f9774
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 20:57:45 +0530

    Test delete button corresponding to each bookmark

--- src/__tests__/DeleteButton.test.js
+++ src/__tests__/DeleteButton.test.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { BookmarkProvider } from '../BookmarkContext';
-import reducer, { initialState } from '../Reducer';
-import Bookmark from '../components/Bookmark';
+import DeleteButton from '../components/DeleteButton';
 
 describe('Bookmark Component', () => {
     test('Check delete button click calls the onClick handler', () => {
         const onClickHandler = jest.fn();
         const wrapper = mount(
-            <BookmarkProvider reducer={reducer} initialState={initialState}>
-                <Bookmark />
-            </BookmarkProvider>
+            <DeleteButton deleteRepo={onClickHandler} id={1234} />
         );
         const button = wrapper.find('button');
-        button.setProps({
-            onClick: onClickHandler
-        });
         button.simulate('click');
         expect(onClickHandler).toHaveBeenCalledTimes(1);
     })
