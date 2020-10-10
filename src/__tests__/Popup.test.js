cfe1ec7b522135d01b6ff1309a9e2df5118dde06
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-10 19:02:54 +0530

    Check if reference captures the title value

--- src/__tests__/Popup.test.js
+++ src/__tests__/Popup.test.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { mount } from 'enzyme';
 import Popup from '../components/Popup';
+
+// Mocking useRef hook
+jest.mock('react', () => {
+    const originReact = jest.requireActual('react');
+    const mockUseRef = jest.fn();
+    return {
+      ...originReact,
+      useRef: mockUseRef,
+    };
+});
 
 describe('Popup Component', () => {
     test('Check if change handler is called', () => {
@@ -10,4 +20,14 @@ describe('Popup Component', () => {
         titleBar.simulate('change');
         expect(changeHandlerMock).toHaveBeenCalledTimes(1);
     })
+
+    test('Checking if the title is captured correctly by reference', () => {
+        const mockRef = { current: { value: '' } };
+        useRef.mockReturnValueOnce(mockRef);
+        const wrapper = mount(<Popup titleRef={mockRef} />);
+        const searchBar = wrapper.find('input');
+        searchBar.instance().value = 'sample title';
+        expect(mockRef.current.value).toEqual('sample title');
+        expect(mockRef.current.value).not.toEqual('popup');
+    });
 })
