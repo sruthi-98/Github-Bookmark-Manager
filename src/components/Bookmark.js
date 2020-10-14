00e5fb3f4293ea5cc11b9c4b5facc6744ac95b86
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-10-14 16:13:46 +0530

    Edit font size

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -14,7 +14,7 @@ function Bookmark({ bookmark }) {
 
     return (
         <div className="bg-white border border-solid border-gray-400 shadow-md p-5 m-5 rounded">
-            {bookmark.title !== '' && <p className="block text-xl text-orange-800 ml-2 font-black">{bookmark.title}</p>}
+            {bookmark.title !== '' && <p className="block text-2xl text-orange-800 ml-2 font-black">{bookmark.title}</p>}
             <div className="block md:flex justify-between items-center">
             <div className="flex flex-col p-2">
                 <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
